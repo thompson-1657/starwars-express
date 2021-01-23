ddbe84d6d302cbe335a87673f7e074dd88f1ecda
Rachel Thompson <thompson.1657@gmail.com>
2021-01-23 11:09:47 -0600

    Added /api/characters route

--- server.js
+++ server.js
@@ -2,8 +2,44 @@ const express = require('express');
 const app = express()
 const PORT = 3000
 
+const characters = [
+    {
+        name: 'Yoda',
+        role: 'Jedi Master',
+        forcePoints: 100000,
+        age: 900,
+        avatar: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/baby-yoda-old-yoda-1574103229.jpg?crop=0.486xw:0.973xh;0.514xw,0&resize=480:*',
+        routeName: 'yoda',
+    },
+    {
+        name: 'Luke Skywalker',
+        role: 'Jedi Master',
+        forcePoints: 10000,
+        age: 40,
+        avatar: 'https://static.wikia.nocookie.net/star-wars-canon-extended/images/2/2c/Luke_Sky7.jpg/revision/latest/scale-to-width-down/340?cb=20180123070942',
+        routeName: 'lukeskywalker',
+    },
+    {
+        name: 'Princess Leia',
+        role: 'General Princess',
+        forcePoints: 100,
+        age: 40,
+        avatar: 'https://api.time.com/wp-content/uploads/2016/12/carrie-fisher-movies-2.jpg?w=600&quality=85',
+        routeName: 'princessleia',
+    },
+]
+
 app.get('/', (req, res) => {
     res.send('May the force be with you!')
+})
+
+// /api/characters- show all character data
+app.get('/api/characters', (req, res) => {
+    res.json(characters)
+})
+// /api/characters/:routeName
+app.get('/api/characters/:routeName', (req, res) => {
+    res.json()
 })
 
 app.listen(PORT, () => {
